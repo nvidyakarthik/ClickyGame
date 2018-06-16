6a829415179dbf774d2c4038b8c80bd3e1cae1ca
nvidya.karthik <nvidya.karthik@gmail.com>
2018-06-16 14:48:23 -0700

    changed GameCard to Gamecard weird error

--- src/components/GameCard/Gamecard.js
+++ src/components/GameCard/Gamecard.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./GameCard.css";
+import "./Gamecard.css";
 
-const GameCard = props => (
+const Gamecard = props => (
   <div className="card" onClick={()=>props.shuffleImage(props.id)}>
     <div className="card-body">
       <div className="img-container">
@@ -11,4 +11,4 @@ const GameCard = props => (
   </div>
 );
 
-export default GameCard;
+export default Gamecard;
